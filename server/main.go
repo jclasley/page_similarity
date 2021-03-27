6ba7a73ce29b7967b3fb8e82e749770dd5445c3d
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-03-27 16:57:20 -0600

    Condensed unmarshaling and created plaintext
    Method for retrieving plaintext from a page given an id
    Changed unmarshal pointer type to be of nested maps instead of going a
    level at a time

--- server/main.go
+++ server/main.go
@@ -12,19 +12,32 @@ import (
 
 func main() {
 	http.HandleFunc("/api/search/", genHandler)
+	http.HandleFunc("/api/extract/", extractHandler) 
 	http.ListenAndServe(":8080", nil)
 }
 
 func genHandler(w http.ResponseWriter, r *http.Request) {
 	t := r.URL.Query().Get("t")
 	fmt.Println("t", t)
-	rs, err := findPage(t)
+	js, err := findPage(t)
 	if err != nil {
 		w.WriteHeader(400)
 		w.Write([]byte(fmt.Sprintf(`%s`, err.Error())))
 	}
 	w.WriteHeader(200)
-	w.Write(rs)
+	w.Write(js)
+}
+
+func extractHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	fmt.Println(id)
+	js, err := plainTextPage(id)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf(`%s`, err.Error())))
+	}
+	w.WriteHeader(200)
+	w.Write(js)
 }
 
 func findPage(t string) (json.RawMessage, error) {
@@ -40,11 +53,30 @@ func findPage(t string) (json.RawMessage, error) {
 		return []byte{}, err
 	}
 
-	var objMap map[string]json.RawMessage
+	var objMap map[string]map[string]json.RawMessage
 	json.Unmarshal(b, &objMap)
-
-	var queryMap map[string]json.RawMessage
-	json.Unmarshal(objMap["query"], &queryMap) // one level down
 	
-	return queryMap["search"], nil
+	return objMap["query"]["search"], nil
 }
+
+func plainTextPage(id string) (json.RawMessage, error) {
+	url := fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=query&format=json&pageids=%s&prop=extracts&explaintext", id)
+	res, err := http.Get(url)
+	fmt.Println(url)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	var js map[string]map[string]map[string]map[string]json.RawMessage
+	json.Unmarshal(b, &js)
+
+	return js["query"]["pages"][id]["extract"], nil
+}
+
+// https://en.wikipedia.org/w/api.php?action=query&pageids=417121&prop=extracts&explaintext
+// 
